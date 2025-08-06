aacf0fb8eeeb8673f5905df78a96ac5575c814d1
gopala <gopal1852000@gmail.com>
2025-08-06 21:10:26 +0530

    input validation added user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import { createUser, createSession } from '../controller/userController';
 import { verifySession } from '../middleware/authMiddleware';
+import { validate_createUser_input, validate_createSession_input  } from '../middleware/inputValidationUser';
+
 
 export const userRouter = Router()
 
-userRouter.post('/signup', createUser);
-userRouter.post('/login', createSession);
+userRouter.post('/signup', validate_createUser_input, createUser);
+userRouter.post('/login', validate_createSession_input, createSession);
